d382304b0241b795eb192c8e4abea339e959b782
Richard <ramasefield@gmail.com>
2016-07-12 08:14:01 +0200

    tuesday

--- dev/assets/js/global.js
+++ dev/assets/js/global.js
@@ -1,6 +1,5 @@
 var searchVisible = false;
 var isMobile = false;
-
 // --------------------------------------------------
 // ----- Global for header, footer, search, etc -----
 // --------------------------------------------------
@@ -14,7 +13,7 @@ if (navigator.userAgent == 'Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 
     $('body').addClass('iemobile9');
 }
 // check is box-sizing is supported
-Modernizr.addTest("boxsizing", function () {
+Modernizr.addTest("boxsizing", function() {
     return Modernizr.testAllProps("boxSizing") && (document.documentMode === undefined || document.documentMode > 7);
 });
 if (Modernizr.boxsizing) {
@@ -25,32 +24,28 @@ if (Modernizr.boxsizing) {
     }
 }
 // check if iOS device
-Modernizr.addTest('isios', function(){
+Modernizr.addTest('isios', function() {
     return navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false
 });
-
 // Global Initialize
-$(document).ready(function () {
-   
-   // bg vid
-    //$('.hero').vide(); // Non declarative initialization
+$(document).ready(function() {
+    // Appliance Scroller
+    checkScrollerSize();
+    
     //Search
-    function setSearchVisible(visible)
-    {
-        if(screenType == "smart")
-                isMobile = true;
-        if (visible == searchVisible)
-            return;
+    function setSearchVisible(visible) {
+        if (screenType == "smart") isMobile = true;
+        if (visible == searchVisible) return;
         searchVisible = visible;
         $(".search-box")[searchVisible ? "addClass" : "removeClass"]("active");
-        if (searchVisible)
-        {
+        if (searchVisible) {
             $(".search-box input").attr("placeholder", isMobile ? "Search..." : "Start typing").val("");
             setTimeout(function() {
                 $(".site-header .search-box input").focus();
             }, 100);
         }
     }
+    // SEARCH BAR
     $("header .search").click(function() {
         setSearchVisible(!searchVisible);
     });
@@ -60,12 +55,10 @@ $(document).ready(function () {
     $("header .search-box .close").click(function() {
         setSearchVisible(false);
     })
-
     $(".menu").click(function(e) {
         e.preventDefault();
         $("header").toggleClass("open");
-    });        
-    
+    });
     // Replace img src based on device screen size
     if ($('img.responsive').length) {
         responsiveImages();
@@ -77,9 +70,10 @@ $(document).ready(function () {
             swapImgSource();
         })
     }
-
     // Additional class support for breakpoints
-    $(window).resize(function () {
+    $(window).resize(function() {
+        //check if we need the scrollers
+        checkScrollerSize();
         // Replace img src based on device screen size
         if ($('img.responsive').length) {
             responsiveImages();
@@ -88,22 +82,44 @@ $(document).ready(function () {
         if ($('img.bg').length) {
             swapImgSource();
         }
-
     });
-
     $(window).trigger('resize');
-
 });
 
+function checkScrollerSize(){
+  
+    app = $('.appliances');
+    row = $('.appliances .row');
+    li = $('.appliances li');
+    ul = $('.appliances ul');
+        if(screenType =="smart"){
+            $eval = (row.width()/3) - 11;
+            if($eval >= 140){
+                $(li).css('width', $eval)
+                $(ul).css('width', $eval*7)
+            }            
+        }else{
+            $(li).css('width', '')
+            $(ul).css('width', '')
+        }
+    resetScreenVars();
+    if(screenType == "smart"){
+        if($(window).width() < $('.appliances ul').width()){
+            $('.arrow').addClass('active');
+        }   
+    }else{
+        if($('.arrow').hasClass('active')) $('.arrow').removeClass('active');
+    }
+}
 // -------- IMAGES --------
 var windowWidth = $(window).width();
 // Replace img src based on device screen size
 var desktopBreak = 1000,
-tabletBreak = 700,
-smartBreak = 320,
-screenWidth = 0,
-screenHeight = 0,
-screenType = 'feature';
+    tabletBreak = 767,
+    smartBreak = 320,
+    screenWidth = 0,
+    screenHeight = 0,
+    screenType = 'feature';
 
 function resetScreenVars() {
     screenWidth = $(window).width();
@@ -121,17 +137,16 @@ function resetScreenVars() {
 
 function responsiveImages() {
     resetScreenVars();
-    $('img.responsive').each(function () {
+    $('img.responsive').each(function() {
         var src = $(this).attr('src');
         src = src.replace(/(feature|smart|tablet|desktop)/, screenType);
-        $(this).attr('src', src);        
+        $(this).attr('src', src);
     });
 }
-
 // Convert inline image to background
 function swapImgSource() {
     resetScreenVars();
-    $('img.bg').each(function () {
+    $('img.bg').each(function() {
         var src = $(this).attr('src');
         var p = $(this).parent();
         $(this).attr('src', src);
